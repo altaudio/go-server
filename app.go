814d1e2761dd2f3341f4f4bcadc8c628c61b9d13
Alex Smith <alex.smith@mubaloo.com>
2019-02-13 15:36:40 +0000

    Adds Gorilla Mux example

--- app.go
+++ app.go
@@ -3,21 +3,40 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	port := ":8080"
-	staticDirectory := "static/"
-	staticURL := "/static/"
+	router := mux.NewRouter()
 
-	fileServer := http.FileServer(http.Dir(staticDirectory))
+	// Example of getting the URL segments
+	router.HandleFunc("/books/{title}/page/{page}", func(responseWriter http.ResponseWriter, request *http.Request) {
+		vars := mux.Vars(request)
+		title := vars["title"]
+		page := vars["page"]
 
-	http.Handle(staticURL, http.StripPrefix(staticURL, fileServer))
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my website: %s", r.URL.Query().Get("name"))
+		fmt.Fprintf(responseWriter, "You've requested the book %s at page %s", title, page)
 	})
 
-	fmt.Printf("Listening on port%s\n", port)
-	http.ListenAndServe(port, nil)
+	// Example of restricting request handlers to HTTP methods
+	router.HandleFunc("/books", func(responseWriter http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(responseWriter, "POST /books Response")
+	}).Methods("POST")
+
+	// Subrouter Example
+	magazineRouter := router.PathPrefix("/magazines").Subrouter()
+	magazineRouter.HandleFunc("/", func(responseWriter http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(responseWriter, "GET /magazines Response")
+	})
+	magazineRouter.HandleFunc("/{title}", func(responseWriter http.ResponseWriter, request *http.Request) {
+		vars := mux.Vars(request)
+		title := vars["title"]
+
+		fmt.Fprintf(responseWriter, "GET /magazines title: %s", title)
+	})
+
+	fmt.Printf("Server listening on port%s", port)
+	http.ListenAndServe(port, router)
 }
